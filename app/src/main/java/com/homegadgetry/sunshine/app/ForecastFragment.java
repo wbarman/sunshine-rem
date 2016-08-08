80263cbd2a7474a4e77152e00a0051a1907f0e63
Jan Baran <baranj.sec@gmail.com>
2016-08-08 11:35:25 +0200

    added SharedPreferences to store user's location

--- app/src/main/java/com/homegadgetry/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/homegadgetry/sunshine/app/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.homegadgetry.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
@@ -54,8 +56,11 @@ public class ForecastFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_refresh) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
+            weatherTask.execute(location);
             return true;
         }
 
