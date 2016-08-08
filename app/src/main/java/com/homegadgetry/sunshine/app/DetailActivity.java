d7a49aa00cdfdbc743a77d6a1f358465464ce3ff
Jan Baran <baranj.sec@gmail.com>
2016-08-08 08:35:34 +0200

    added SettingsActivity; added Menu to DetailActivity; run SettingsActivity from DetailActivity and MainActivity

--- app/src/main/java/com/homegadgetry/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/homegadgetry/sunshine/app/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.homegadgetry.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -16,4 +19,20 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings){
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
